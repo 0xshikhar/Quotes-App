34a5bf22965688991b84115999ec983180497671
0xshikhar <shikhar@iiitmanipur.ac.in>
2023-07-03 03:39:43 +0530

    fetching api quote data based on input number

--- src/components/application.tsx
+++ src/components/application.tsx
@@ -3,6 +3,12 @@ import Quotes from './quotes';
 import InspirationalQuote from './quote';
 import Loading from './loading';
 
+
+// The form for changing the count and submitting the form is in the <Quotes> component.
+// You'll need to pass in the count as well as event handlers for onChange and onSubmit.
+// Add the correct typing for the children in the <Quotes /> component.
+// Add the correct typing for the onSubmit event handler.
+
 export type Quote = {
   id: number;
   content: string;
@@ -17,23 +23,32 @@ export const fetchRandomQuote = async () => {
 export const fetchQuotes = async (count: number) => {
   const response = await fetch(`/api/quotes?limit=${count}`);
   return response.json();
-};
+}
 
 const Application = () => {
   const [quote, setQuote] = useState<Quote>();
+  const [quotes, setQuotes] = useState<Quote[]>([]);
+  const [count, setCount] = useState(10);
 
   useEffect(() => {
     fetchRandomQuote().then(setQuote);
   }, []);
 
-  if (!quote) return <Loading />;
+  useEffect(() => {
+    fetchQuotes(count).then(setQuotes);
+  }, [count]);
+
   return (
     <main className="w-full max-w-2xl py-16 mx-auto">
-      <InspirationalQuote content={quote.content} source={quote.source} />
-      <Quotes>
-        <div className="grid grid-cols-2 gap-4">
-          <InspirationalQuote content={quote.content} source={quote.source} />
-        </div>
+      {/* <InspirationalQuote content={quote.content} source={quote.source} /> */}
+      <Quotes onSubmit={() => fetchQuotes(count)} count={count} onChange={(e) => setCount(+e.target.value)} >
+        {quotes.map((quote) => {
+          return (
+            <div key={quote.id}>
+              <InspirationalQuote key={quote.id} content={quote.content} source={quote.source} />
+            </div>
+          )
+        })}
       </Quotes>
     </main>
   );
--- src/components/quotes.tsx
+++ src/components/quotes.tsx
@@ -1,36 +1,44 @@
-import { useState, ReactNode } from 'react';
+import { useState, ReactNode, PropsWithChildren } from 'react';
 
-const Quotes = ({ children, count, onSubmit, onChange }: { React.ReactNode, number, React.FormEventHandler<HtmlFormHandler> , number}) => {
+type QuotesProps = {
+  children: ReactNode;
+  count: number;
+  onSubmit: React.FormEventHandler<HTMLFormElement>;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+const Quotes = ({ children, count, onSubmit, onChange }: PropsWithChildren<QuotesProps>) => {
+  //  { React.ReactNode, number, React.FormEventHandler<HtmlFormHandler> , number}
   const [numberOfQuotes, setNumberOfQuotes] = useState(count);
 
   return (
-  <section className="flex flex-col gap-8">
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        setNumberOfQuotes(count);
-        // onChange(e);
-      }}
-    >
-      <label htmlFor="number-of-quotes-to-load" className="block">
-        Number of Quotes to Load
-      </label>
-      <div className="flex">
-        <input
-          id="number-of-quotes-to-load"
-          className="w-full"
-          type="number"
-          min="0"
-          max="25"
-          value={count}
-          onChange={onChange}
-        />
-        <button type="submit">Load Quotes</button>
-      </div>
-    </form>
-    <div className="grid grid-cols-2 gap-4">{children}</div>
-  </section>
+    <section className="flex flex-col gap-8">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setNumberOfQuotes(count);
+          // onChange(e);
+        }}
+      >
+        <label htmlFor="number-of-quotes-to-load" className="block">
+          Number of Quotes to Load
+        </label>
+        <div className="flex">
+          <input
+            id="number-of-quotes-to-load"
+            className="w-full"
+            type="number"
+            min="0"
+            max="25"
+            value={count}
+            onChange={onChange}
+          />
+          <button type="submit">Load Quotes</button>
+        </div>
+      </form>
+      <div className="grid grid-cols-2 gap-4">{children}</div>
+    </section>
   );
 };
 
-  export default Quotes;
+export default Quotes;
